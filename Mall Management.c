796af2140063658712bcf53793bf09148281e639
Kanisha-Shah <82677033+Kanisha-Shah@users.noreply.github.com>
2021-11-14 10:53:23 +0530

    Add files via upload

---
+++ Mall Management.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void wel_come(void)
+
+{
+	setcolor(11);
+	time_t t;
+	time(&t);
+	printf("                                                                                                         \n");
+	printf("---------------------------------------------------------------------------------------------------------\n");
+	printf("\t\t\t\t\t%s",ctime(&t));
+	printf("---------------------------------------------------------------------------------------------------------\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t**********************************\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t*\t     WELCOME TO \t *\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t*\tNEXT SHOPPING CENTER\t *\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t**********************************\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t      Pokhara, Savagriha Chowk\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t    061567890\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t      \"WE BELIEVE IN QUALITY\"\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+	printf("|Press any key to continue.........\t\t\t\t\t\t\t\t\t\t|\n");
+
+	printf("---------------------------------------------------------------------------------------------------------\n");
+
+getch();
+system("cls");
+}
+void main()
+{
+
+}
